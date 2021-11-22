71ab87c34526d3d31eaea602fc21711187a9c77d
THARUNESHWAR S <s.tharuneshwar.369@gmail.com>
2021-11-22 23:36:01 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from HAND_GESTURE_MEDIAPLAYER_CONTROLER import YT_VLC_CONTROL_HAND_TRACKING
 
 if __name__ == "__main__":
-    controler = YT_VLC_CONTROLL_HAND_TRACKING(CHOICE='vlc', WEB_CAM_SOURCE=0)
-    controler.start()
+    run = YT_VLC_CONTROL_HAND_TRACKING(CHOICE='youtube', 
+    swipe=False)
+    run.start()
