1c614e4f641bb84dffbef010ba13131d2a160fd8
THARUNESHWAR S <s.tharuneshwar.369@gmail.com>
2022-06-02 17:53:30 +0530

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-from HAND_GESTURE_MEDIAPLAYER_CONTROLER import YT_VLC_CONTROL_HAND_TRACKING
-
-if __name__ == "__main__":
-    run = YT_VLC_CONTROL_HAND_TRACKING(CHOICE='youtube', 
-    swipe=False)
-    run.start()
